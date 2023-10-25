4594909f6c45155129905ae654763f13bd3b265e
SiCuellar <cuellarsilvestre@gmail.com>
2023-10-25 12:36:46 -0700

    ng model change event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,18 @@ import { WishItem } from 'src/shared/models/wishItem';
 })
 export class AppComponent {
   items : WishItem[] = [
-    // new WishItem('To learn Angular'), 
-    // new WishItem('Get Coffee', true),
-    // new WishItem('Find grass that cuts itself')
+    new WishItem('To learn Angular'), 
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself')
   ]
 
-  newWishText = ''
+  listFilter : String = '0';
+  
+  newWishText = '';
   
   title = 'my wishlist';
+
+  visableItems : WishItem[] = this.items;
 
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
@@ -23,6 +27,16 @@ export class AppComponent {
     //todo :add wish to items array
     // clear the text box
 
+  }
+
+  filterChanged(value: any) {
+    if (value === '0') {
+      this.visableItems = this.items;
+    } else if (value === '1') {
+      this.visableItems = this.items.filter(item => !item.isComplete)
+    } else {
+      this.visableItems = this.items.filter(item => item.isComplete)
+    }
   }
 
   toggleItem(item : WishItem) {
