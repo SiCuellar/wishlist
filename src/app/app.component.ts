746cd039770d3a043bcde6bc45372d6fb88c1752
SiCuellar <cuellarsilvestre@gmail.com>
2023-10-25 11:59:13 -0700

    adds form using ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,22 @@ import { WishItem } from 'src/shared/models/wishItem';
 })
 export class AppComponent {
   items : WishItem[] = [
-    new WishItem('To learn Angular'), 
-    new WishItem('Get Coffee', true),
-    new WishItem('Find grass that cuts itself')
+    // new WishItem('To learn Angular'), 
+    // new WishItem('Get Coffee', true),
+    // new WishItem('Find grass that cuts itself')
   ]
+
+  newWishText = ''
+  
   title = 'my wishlist';
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+    //todo :add wish to items array
+    // clear the text box
+
+  }
 
   toggleItem(item : WishItem) {
     item.isComplete = !item.isComplete;
