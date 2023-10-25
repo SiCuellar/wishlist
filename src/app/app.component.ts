3917542b0de11c0cbf434f2da7a10e0a13b32d83
SiCuellar <cuellarsilvestre@gmail.com>
2023-10-25 13:31:34 -0700

    adds getter method for filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { WishItem } from 'src/shared/models/wishItem';
 
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete,
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,31 +19,33 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself')
   ]
 
-  listFilter : String = '0';
+  listFilter : any = '0';
   
   newWishText = '';
   
   title = 'my wishlist';
 
-  visableItems : WishItem[] = this.items;
+  // visableItems : WishItem[] = this.items;
+
+  get visableItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+  };
 
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
-    //todo :add wish to items array
-    // clear the text box
-
   }
 
-  filterChanged(value: any) {
-    if (value === '0') {
-      this.visableItems = this.items;
-    } else if (value === '1') {
-      this.visableItems = this.items.filter(item => !item.isComplete)
-    } else {
-      this.visableItems = this.items.filter(item => item.isComplete)
-    }
-  }
+  // Setting get visable Items above allows us to get rid of this method
+  // filterChanged(value: any) {
+    // if (value === '0') {
+    //   this.visableItems = this.items;
+    // } else if (value === '1') {
+    //   this.visableItems = this.items.filter(item => !item.isComplete)
+    // } else {
+    //   this.visableItems = this.items.filter(item => item.isComplete)
+    // }
+  // }
 
   toggleItem(item : WishItem) {
     item.isComplete = !item.isComplete;
